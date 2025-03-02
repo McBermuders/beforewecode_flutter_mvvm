bcc9e78088442c031155f93ef9f094983fef52c1
Tim Muders <tim.muders@googlemail.com>
2025-03-02 11:06:44 -0600

    added listview on start to open each coordinator directly

--- mvvm_tutorial_one_beforewecode/lib/main.dart
+++ mvvm_tutorial_one_beforewecode/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_tutorial_one_beforewecode/app/business/coordinators/root_coordinator.dart';
+import 'package:mvvm_tutorial_one_beforewecode/app/business/coordinators/test_coordinator.dart';
 
+import 'app/business/coordinators/detail_coordinator.dart';
+import 'app/business/coordinators/form_coordinator.dart';
 import 'app/business/coordinators/login_coordinator.dart';
+import 'core/contracts/coordinators/coordinator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +22,46 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginCoordinator().start(),
+      home: const CoordinatorListWidget(),
     );
+  }
+}
+
+class CoordinatorListWidget extends StatelessWidget {
+  const CoordinatorListWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final coordinators = getCoordinators();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Coordinators'),
+      ),
+      body: ListView.builder(
+        itemCount: coordinators.length,
+        itemBuilder: (context, index) {
+          final coordinator = coordinators[index];
+          return ListTile(
+            title: Text(coordinator.runtimeType.toString()),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => coordinator.start()),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  List<Coordinator> getCoordinators() {
+    return [
+      RootCoordinator(),
+      TestCoordinator(),
+      DetailCoordinator(),
+      FormCoordinator(),
+      LoginCoordinator(),
+    ];
   }
 }
